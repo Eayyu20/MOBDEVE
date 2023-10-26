e2406b86677d54ff51773bfe1903eea57f870c2c
Eayyu20 <ethan_yu@dlsu.edu.ph>
2023-10-26 13:09:58 +0800

    Base Activity Files

---
+++ app/src/main/java/com/example/mobdeve/CharacterSelectActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobdeve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterSelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.character_select)
+    }
+}

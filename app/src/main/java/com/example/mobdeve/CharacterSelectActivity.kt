e44ad1ed3ab7c308531d0fd6703b4b6f3804a5af
lgltan <lgltan20@gmail.com>
2023-10-28 14:39:04 +0800

    added char select and game logic, incomplete

--- app/src/main/java/com/example/mobdeve/CharacterSelectActivity.kt
+++ app/src/main/java/com/example/mobdeve/CharacterSelectActivity.kt
@@ -1,14 +1,48 @@
 package com.example.mobdeve
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class CharacterSelectActivity : AppCompatActivity() {
+    private var p1CharSelect: Int = -1
+    private var p2CharSelect: Int = -1
+
+    private var p1LockStatus: Int = -1
+    private var p2LockStatus: Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.character_select)
 
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+
+    fun onCharSelect(charId: Int) {
+        // if player is player 1
+        if (true) {
+            p1CharSelect = charId
+        }
+        // if player is player 2
+        else {
+            p2CharSelect = charId
+        }
+    }
+    fun onLock() {
+        // set player lock status to 1
+        if (true) {
+            p1LockStatus = 1
+        }
+        else {
+            p2LockStatus = 1
+        }
+
+        if (p1LockStatus == 1 && p2LockStatus == 1) {
+            val i = Intent(this, CharacterSelectActivity::class.java)
+            i.putExtra("p1Char", p1CharSelect)
+            i.putExtra("p2Char", p2CharSelect)
+            startActivity(i)
+        }
     }
 }

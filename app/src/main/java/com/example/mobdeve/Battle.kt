f280aeedab8559b1409a830b96f2638d2d3b01f5
lgltan <lgltan20@gmail.com>
2023-12-02 15:29:44 +0800

    moved isCollision to battle

--- app/src/main/java/com/example/mobdeve/Battle.kt
+++ app/src/main/java/com/example/mobdeve/Battle.kt
@@ -1,5 +1,10 @@
 package com.example.mobdeve
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Rect
+
 class Battle(var p1: Player, var p2: Player) {
     var stage_size_x = 0
     var stage_size_y = 0
@@ -19,5 +24,46 @@ class Battle(var p1: Player, var p2: Player) {
 
         this.p1.actionFrame = 0 // frame count
         this.p1.actionFrame = 0 // frame count
+    }
+
+    fun isColliding(context: Context): Boolean{
+
+        val sprite1X = this.p1.posX
+        val sprite1Y = this.p1.posY
+
+        val sprite2X = this.p2.posX
+        val sprite2Y = this.p2.posY
+
+        val player1Bitmap: Bitmap
+        val player2Bitmap: Bitmap
+
+        // get player 1 attack sprite dimensions
+        if(this.p1.charId == 1){
+            player1Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.standing_1)
+        }else if(this.p1.charId == 2){
+            player1Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.standing_2)
+        }else{
+            player1Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.standing_3)
+        }
+
+        // get player 2 attack sprite dimensions
+        if(this.p2.charId == 1){
+            player2Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.standing_1)
+        }else if(this.p2.charId == 2){
+            player2Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.standing_2)
+        }else{
+            player2Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.standing_3)
+        }
+
+        val sprite1Height = player1Bitmap.height
+        val sprite1Width = player1Bitmap.width
+
+        val sprite2Height = player2Bitmap.height
+        val sprite2Width = player2Bitmap.width
+
+        val rectSprite1 = Rect(sprite1X, sprite1Y, sprite1X + sprite1Width, sprite1Y + sprite1Height)
+        val rectSprite2 = Rect(sprite2X, sprite2Y, sprite2X + sprite2Width, sprite2Y + sprite2Height)
+
+        return Rect.intersects(rectSprite1, rectSprite2)
     }
 }

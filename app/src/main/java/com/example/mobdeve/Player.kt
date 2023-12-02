f860bc45c8c6b2a078d7c878791c6273034f53c5
lgltan <lgltan20@gmail.com>
2023-12-02 12:59:24 +0800

    updated player variables (2)

--- app/src/main/java/com/example/mobdeve/Player.kt
+++ app/src/main/java/com/example/mobdeve/Player.kt
@@ -5,9 +5,9 @@ class Player {
     var hp: Int = 100
     var posX: Float = 0F
     var posY: Float = 0F
-    var currentAction = "idle"
+    var currentAction : Int = 0 // 0 - idle, 1 - moving, 2 - normal attack, 3 - special attack, 4 - death
     var actionFrame: Int = 0
-    var charId = 0
+    var charId : Int = 0 // 1 - sword, 2 - spear, 3 - shield
     lateinit var normal_attack : Attack
     lateinit var special_attack : Attack
 

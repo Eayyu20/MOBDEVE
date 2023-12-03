7b83d12c664dc045bc50cd57c686ea1d3a0cebc9
Rafu <ralph_furigay@dlsu.edu.ph>
2023-12-03 21:06:56 +0800

    arena init

---
+++ app/src/main/java/com/example/mobdeve/Arena.kt
@@ -0,0 +1,41 @@
+package com.example.mobdeve
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.Log
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class Arena(context: Context, attrs: AttributeSet): SurfaceView(context, attrs), SurfaceHolder.Callback {
+
+    init{
+        holder.addCallback(this)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        // Handle surface changes if needed
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        // Handle surface destruction if nee
+    }
+
+//    fun renderCanvas(bitmap: Bitmap) {
+//        val surfaceCanvas = holder.lockCanvas()
+//        surfaceCanvas.drawBitmap(bitmap, 0f, 0f, null)
+//        holder.unlockCanvasAndPost(surfaceCanvas)
+//    }
+}

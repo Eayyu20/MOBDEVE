85d3500672b3f7a1910d4e5a6b55c1f46d6dfe1d
lgltan <lgltan20@gmail.com>
2023-12-04 01:44:22 +0800

    updating spritesheet

--- app/src/main/java/com/example/mobdeve/SpriteSheet.kt
+++ app/src/main/java/com/example/mobdeve/SpriteSheet.kt
@@ -28,30 +28,7 @@ class SpriteSheet (context: Context, char_type: Int){
         }
     }
 
-    fun getBM(): Bitmap {
-        return this.bitmap
-    }
-    fun getSprite(idxRow: Int, idxCol: Int): Sprite {
-        return Sprite(this, Rect(idxCol * SPRITE_WIDTH_PIXELS, idxRow * SPRITE_HEIGHT_PIXELS,(idxCol + 1) * SPRITE_WIDTH_PIXELS,(idxRow + 1) * SPRITE_HEIGHT_PIXELS))
-    }
-
-    fun getIdleSprite(frame_num: Int): Sprite {
-        return Sprite(this, Rect(frame_num * SPRITE_WIDTH_PIXELS, 0  * SPRITE_HEIGHT_PIXELS, (frame_num + 1) * SPRITE_WIDTH_PIXELS, (0 + 1) * SPRITE_HEIGHT_PIXELS))
-    }
-
-    fun getMovingSprite(frame_num: Int): Sprite {
-        return Sprite(this, Rect(frame_num * SPRITE_WIDTH_PIXELS, 1  * SPRITE_HEIGHT_PIXELS, (frame_num + 1) * SPRITE_WIDTH_PIXELS, (1 + 1) * SPRITE_HEIGHT_PIXELS))
-    }
-
-    fun getNormalAttackSprite(frame_num: Int): Sprite {
-        return Sprite(this, Rect(frame_num * SPRITE_WIDTH_PIXELS, 2  * SPRITE_HEIGHT_PIXELS, (frame_num + 1) * SPRITE_WIDTH_PIXELS, (2 + 1) * SPRITE_HEIGHT_PIXELS))
-    }
-
-    fun getSpecialAttackSprite(frame_num: Int): Sprite {
-        return Sprite(this, Rect(frame_num * SPRITE_WIDTH_PIXELS, 3  * SPRITE_HEIGHT_PIXELS, (frame_num + 1) * SPRITE_WIDTH_PIXELS, (3 + 1) * SPRITE_HEIGHT_PIXELS))
-    }
-
-    fun getDeathSprite(frame_num: Int): Sprite {
-        return Sprite(this, Rect(frame_num * SPRITE_WIDTH_PIXELS, 4  * SPRITE_HEIGHT_PIXELS, (frame_num + 1) * SPRITE_WIDTH_PIXELS, (4 + 1) * SPRITE_HEIGHT_PIXELS))
+    fun getSprite(idxRow: Int, idxCol: Int): Bitmap {
+        return Bitmap.createBitmap(bitmap, idxCol * SPRITE_WIDTH_PIXELS, idxRow * SPRITE_HEIGHT_PIXELS, SPRITE_WIDTH_PIXELS, SPRITE_HEIGHT_PIXELS)
     }
 }
